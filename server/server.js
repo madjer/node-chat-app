5ee96f07b35ad1e6304d3b50e7f400de6f3c9695
Madjer <madjerbo@gmail.com>
2018-03-22 21:11:30 -0300

    Emit newMessage on createMessage

--- server/server.js
+++ server/server.js
@@ -14,18 +14,18 @@ app.use(express.static(publicPath));
 
 io.on('connection', (socket) => {
     console.log('New user connected');
-    socket.emit('newMessage', {
-        from: 'Madjer',
-        text: 'See tou then',
-        createdAt: 123123
+
+    socket.on('createMessage', (message) => {
+        console.log('createMessage', message);
+        io.emit('newMessage', {
+            from: message.from,
+            text: message.text,
+            createdAt: new Date().getTime()
+        })
     })
     socket.on('disconnect', () => {
         console.log('User was disconnected');
     });
-
-    socket.on('createMessage', (message) => {
-        console.log('createMessage', message);
-    })
 });
 
 server.listen(port, () =>{
